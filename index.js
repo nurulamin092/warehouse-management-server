281f75288f8839b642275c6d76887e4c33aeb725
nurulamin092 <mnurulamin092@gmial.com>
2022-05-08 09:33:13 +0600

    async fun convert

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qo3jo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    console.log('Warehouse db connected ');
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db('warehouseDb').collection('product');
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
     res.send('Running warehouse server');
 });
