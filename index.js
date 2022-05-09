8c8bd350c9bbdd9a283b906c57fbd93c7bf0991c
nurulamin092 <mnurulamin092@gmial.com>
2022-05-09 17:34:47 +0600

    delete api

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ async function run() {
             const result = await productsCollection.insertOne(addNewItem);
             res.send(result);
         });
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productsCollection.deleteOne(query);
+            res.send(result)
+        })
     }
     finally {
 
